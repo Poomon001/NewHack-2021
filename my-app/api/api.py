ece3431b118c76220bbcc616619e67678d13f9d8
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2021-11-06 12:00:19 -0700

    add requeiment

--- my-app/api/api.py
+++ my-app/api/api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, json
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build")
 cors = CORS(app)
 
 @app.route("/myAPI", methods=['GET', "POST"])
