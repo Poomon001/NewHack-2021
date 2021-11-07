a19e8c8cecb39bc2111ab208d71d7c98023f9f15
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2021-11-06 18:25:03 -0700

    Add files via upload

--- my-app/src/Pages/main.js
+++ my-app/src/Pages/main.js
@@ -1,27 +1,32 @@
 document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
     const dropZoneElement = inputElement.closest(".drop-zone");
 
+    //when you click on the text box to upload
     dropZoneElement.addEventListener("click", e => {
        inputElement.click();
     });
 
+    //only get the first file from bunch of many you are trying to upload at once
     inputElement.addEventListener("change", e => {
        if (inputElement.files.length){
            updateThumbnail(dropZoneElement, inputElement.files[0]);
        }
     });
 
+    //lets you drag over the text box
     dropZoneElement.addEventListener("dragover", e => {
         e.preventDefault();
         dropZoneElement.classList.add("drop-zone--over");
     });
 
+    //when you drop the file remove the text inside the text box
     ["dragleave", "dragend"].forEach(type => {
         dropZoneElement.addEventListener(type, e => {
             dropZoneElement.classList.remove("drop-zone--over");
         });
     });
 
+    //drop the file into the text box, make sure the first once is dropped
     dropZoneElement.addEventListener("drop", e => {
         e.preventDefault();
 
@@ -31,6 +36,20 @@ document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
         }
 
         dropZoneElement.classList.remove("drop-zone--over");
+        
+        const [file] = document.querySelector('input[type=file]').files;
+        const reader = new FileReader();
+
+        //load the file data into console
+        reader.addEventListener("load", () => {
+            // this will then display a text file
+            console.log(reader.result)
+            // content.innerHTML
+        }, false);
+
+        if (file) {
+            reader.readAsText(file);
+        }
     });
 });
 
@@ -52,7 +71,6 @@ function updateThumbnail(dropZoneElement, file){
         dropZoneElement.querySelector(".drop-zone__prompt").remove();
     }
 
-
     //no thumbnail element at start
     if (!thumbnailElement){
         thumbnailElement = document.createElement("div");
@@ -60,6 +78,7 @@ function updateThumbnail(dropZoneElement, file){
         dropZoneElement.appendChild(thumbnailElement);
     }
 
+    //shows up the file name when uploaded
     thumbnailElement.dataset.label = file.name;
 }
 
@@ -68,13 +87,25 @@ function previewFile() {
     const [file] = document.querySelector('input[type=file]').files;
     const reader = new FileReader();
 
+    //load the file data into console
     reader.addEventListener("load", () => {
         // this will then display a text file
         console.log(reader.result)
+        // content.innerHTML
     }, false);
 
     if (file) {
         reader.readAsText(file);
     }
+
+    // reader.addEventListener('progress', (event) => {
+    //     if (event.loaded && event.total){
+    //         const percent = (event.loaded / event.total) * 100;
+    //         content.innerHTML = `Progress: ${Math.round(percent)}`
+    //         console.log(`Progress: ${Math.round(percent)}`);
+    //     }
+    // });
 }
+
+
 
