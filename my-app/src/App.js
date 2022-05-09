2758fc4710e32425255408134c93d01ed735dcf6
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2022-05-08 17:27:05 -0700

    first deploy

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ import SimpleReactLightbox from "simple-react-lightbox";
 function App() {
   return (
     <div className="App">
+      <title>Phishing Evaluator</title>
       <LightboxProvider>
         <ResultProvider>
           <Lightbox>
